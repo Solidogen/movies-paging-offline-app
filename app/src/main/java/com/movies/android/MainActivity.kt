a5228df57b4141897123167f03da8d285f28a8d2
solidogen <solidogen@gmail.com>
2021-03-17 19:55:21 +0100

    Nav component with multiple backstack, viewbinding

--- app/src/main/java/com/movies/android/MainActivity.kt
+++ app/src/main/java/com/movies/android/MainActivity.kt
@@ -2,12 +2,48 @@ package com.movies.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.movies.android.util.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
     }
-    
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        val navGraphIds = listOf(R.navigation.movies_graph, R.navigation.profile_graph, R.navigation.settings_graph)
+
+        val controller = bottomNavigationView.setupWithNavController(
+                navGraphIds = navGraphIds,
+                fragmentManager = supportFragmentManager,
+                containerId = R.id.navHostContainer,
+                intent = intent
+        )
+
+        controller.observe(this, { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
+    }
 }
